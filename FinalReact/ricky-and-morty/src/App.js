5d251b9d293c64c340e84bb9b4051a25e78e9a81
Maidevs <socialmedia.by.by.mai@gmail.com>
2023-03-28 14:57:13 +0200

    first commit

---
+++ FinalReact/ricky-and-morty/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// Base url: https://rickandmortyapi.com/api
+function App() {
+  return (
+    <nav className='navbar navbar-dark bg-dark'>
+    <div className="App">
+        <a href="/">
+      <h1>Ricky and Morty App</h1>
+      </a>
+    </div>
+    </nav>
+  );
+}
+
+export default App;
---
+++ FinalReact/ricky-and-morty/src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navigation = ({brand}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navigation
